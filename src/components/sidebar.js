1705846c52d346008d4b58fa5348f45688529f72
gunners6518 <gunners6518@gmail.com>
2022-02-15 00:01:37 +0900

    add tailwind &  fix container width

---
+++ src/components/sidebar.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Bio from "../components/bio"
+
+const Sidebar = () => {
+  return (
+    <div>
+      <Bio />
+      <div>カテゴリー</div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const aboutPage = () => {
+  return (
+    <div>
+      <p>てりーの経歴</p>
+    </div>
+  )
+}
+
+export default aboutPage
