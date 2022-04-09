3a53f56ecd75ad0e5260c2f0675b0ea3cfa35ec9
gunners6518 <gunners6518@gmail.com>
2022-04-09 18:03:25 +0900

    fix-p-margin

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,9 +3,7 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 // normalize CSS across browsers
 import "./src/styles/normalize.css"
-// custom CSS styles
-import "./src/styles/style.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism-tomorrow.css"
-import './src/styles/global.css'
+import "./src/styles/global.css"
